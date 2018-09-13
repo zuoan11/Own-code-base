695b0de547dd4952885257e774e41f2c6743e0b2
renyu <renyu@donews.com>
2018-09-13 10:51:50 +0800

    php_exec_ping

---
+++ PHP/知识点/执行第三方的命令_exec/exec_ping.php
@@ -0,0 +1,39 @@
+ <?php
+/**
+*@param $ip target ip
+*@param $times ping times
+*/
+function ping($ip,$times=4)
+{  
+  $info = array();
+  if(!is_numeric($times) ||  $times-4<0)
+  {
+    $times = 4;
+  }
+  /**
+   * PATH_SEPARATOR  ['windows' => ';', 'linux' => ':']
+   * DIRECTORY_SEPARATOR 自适应分隔符
+   */
+  if (PATH_SEPARATOR==':' || DIRECTORY_SEPARATOR=='/')//linux
+  {
+    exec("ping $ip -c $times",$info);
+    if (count($info) < 9)
+    {
+      $info['error']='timeout';
+    }
+  }
+  else //windows
+  {
+    exec("ping $ip -n $times",$info);
+    if (count($info) < 10)
+    {
+      $info['error']='timeout';
+    }
+  }
+  return $info;
+}
+// $ip = '116.31.103.102';//IP地址
+// $ip = '192.168.71.28';//IP地址
+$ip = '120.31.70.142';
+print_r(ping($ip));
+?>
