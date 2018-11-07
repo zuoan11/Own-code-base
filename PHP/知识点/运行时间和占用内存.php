e7a779a039a05d83f8258fcbd942805da548ac4c
renyu <renyu@donews.com>
2018-11-07 17:28:12 +0800

    新增打印花费时间的方式

--- PHP/知识点/运行时间和占用内存.php
+++ PHP/知识点/运行时间和占用内存.php
@@ -9,3 +9,5 @@ $t2 = microtime(true);
 echo '耗时'.round($t2-$t1,3).'秒<br>';
 echo 'Now memory_get_usage: ' . memory_get_usage() . '<br />';
 
+Log::info('耗时'.round($t2-$t1,3).'秒');
+Log::info('Now memory_get_usage: ' . memory_get_usage());
